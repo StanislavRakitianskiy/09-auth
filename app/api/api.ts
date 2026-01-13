8ba76e3cae75b758218afb1983541e6cd1d30b69
Stanislav Rakitianskyi <raketaswig@gmail.com>
2026-01-13 10:30:52 +0200

    delete logErrorResponse api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -4,12 +4,3 @@ export const api = axios.create({
   baseURL: "https://notehub-api.goit.study",
   withCredentials: true,
 });
-
-export const logErrorResponse = (error: unknown) => {
-  if (axios.isAxiosError(error)) {
-    console.error(error.response?.data ?? error.message);
-    return;
-      }
-
-  console.error(error);
-};
